ff4790e14975e03c9c447f9d67d60ec57a935662
Eng-Mohamed-Osama <eng.muhamed.osama@gmail.com>
2021-11-12 14:33:18 +0400

    minor fixes
    adding back-end for the api calls , fixing bugs , deleting un used files

--- src/api/exchangeApi.js
+++
@@ -1,8 +0,0 @@
-export const exchangeOptions = {
-  method: "GET",
-  url: "https://cors-anywhere.herokuapp.com/https://api.coinranking.com/v2/exchanges",
-  headers: {
-    "x-access-token": process.env.REACT_APP_CRYPTO_API,
-    "X-Requested-With": "XMLHttpRequest",
-  },
-};
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,13 +6,14 @@ import {
   FETCH_COIN_CHART,
   FETCH_EXCHANGE,
 } from "./type";
-import { options } from "./../api/cryptoApi";
 import { newsOption } from "./../api/newsApi";
 import { fetchData } from "../api/utils";
-import { exchangeOptions } from "../api/exchangeApi";
+import axios from "axios";
 
 export const fetchMarketState = () => async (dispatch) => {
-  const res = await fetchData(options("/coins"));
+  const res = await axios.get("https://crypto-api-693.herokuapp.com/api", {
+    params: { url: "coins" },
+  });
   dispatch({ type: FETCH_STATE, payload: res.data });
 };
 
@@ -23,20 +24,24 @@ export const fetchMarketNews = (url) => async (dispatch) => {
 };
 
 export const fetchCoinDetails = (coinId) => async (dispatch) => {
-  const res = await fetchData(options(`/coin/${coinId}`));
+  const res = await axios.get(
+    `https://crypto-api-693.herokuapp.com/api/coin/${coinId}`
+  );
 
   dispatch({ type: FETCH_COIN_DETAILS, payload: res.data.coin });
 };
 
 export const fetchCoinChart = (coinId, timeperiod) => async (dispatch) => {
-  const res = await fetchData(
-    options(`/coin/${coinId}/history/?timePeriod=${timeperiod}`)
+  const res = await axios.get(
+    `https://crypto-api-693.herokuapp.com/api/coin/${coinId}/history?timeperiod=${timeperiod}`
   );
   dispatch({ type: FETCH_COIN_CHART, payload: res.data });
 };
 
 export const fetchExchanges = () => async (dispatch) => {
-  const res = await fetchData(exchangeOptions);
+  const res = await axios.get(
+    "https://crypto-api-693.herokuapp.com/api/exchange"
+  );
   dispatch({ type: FETCH_EXCHANGE, payload: res.data });
 };
 
